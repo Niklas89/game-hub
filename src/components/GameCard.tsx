0666bd532ddd404a42952720242b1b4ef1209e93
Niklas <niklas_edelstam@hotmail.com>
2023-06-01 17:15:05 +0200

    Refactor: remove duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,8 +10,7 @@ interface Props {
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
-      {/* overflow:hidden is needed, otherwise border radius on the top is not applied on the image */}
+    <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const GameCardContainer = ({ children }: Props) => {
+  return (
+    <Box width="300px" borderRadius={10} overflow="hidden">
+      {/* overflow:hidden is needed, otherwise border radius on the top is not applied on the image */}
+      {children}
+    </Box> // Box returns a div in chakra when rendered
+  );
+};
+
+export default GameCardContainer;
