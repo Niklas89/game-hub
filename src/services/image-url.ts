4999f78ef4331d6fcb0446b348afdca824f046d0
Niklas <niklas_edelstam@hotmail.com>
2023-06-01 15:49:21 +0200

    Get optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,13 @@
+//import noImage from '../assets/no-image-placeholder.webp';
+
+const getCroppedImageUrl = (url: string) => {
+  //if (!url) return noImage;
+  
+  const target = 'media/';
+  // we want : media/crop/600/400/
+  const index = url.indexOf(target) + target.length;
+  // url.slice(0, index): from the beginning to the index, url.slice(index): from the index to the end
+  return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+}
+
+export default getCroppedImageUrl;
