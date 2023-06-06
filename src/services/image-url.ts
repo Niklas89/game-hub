4cad363eb71d72d0d0867442eaa651df1e47109b
Niklas <niklas_edelstam@hotmail.com>
2023-06-06 11:07:10 +0200

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,8 +1,7 @@
-//import noImage from '../assets/no-image-placeholder.webp';
+import noImage from '../assets/no-image-placeholder.webp';
 
 const getCroppedImageUrl = (url: string) => {
-  //if (!url) return noImage;
-  if (!url) return "";
+  if (!url) return noImage;
   
   const target = 'media/';
   // we want : media/crop/600/400/
