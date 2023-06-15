f1452d9510668daf3c757d5efd2c9dba12b0eb4d
Niklas <niklas_edelstam@hotmail.com>
2023-06-15 12:48:34 +0200

    Fetching games with React Query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
-import useData, { FetchResponse } from "./useData";
+import { FetchResponse } from "./useData";
 import apiClient from "../services/api-client";
 /* import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import { FetchResponse } from "./useData";
+import apiClient from "../services/api-client";
+import platforms from "../data/platforms";
 
 interface Platform {
     id: number;
@@ -6,6 +9,14 @@ interface Platform {
     slug: string;
 }
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+// const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+
+const usePlatforms = () => useQuery({
+    queryKey: ["platforms"],
+    queryFn: () => apiClient.get<FetchResponse<Platform>>("/platforms/lists/parents").then(res => res.data),
+    
+  staleTime: 24 * 60 * 60 * 1000, // platforms will be stored for 24hrs in the cache
+  initialData: {count: platforms.length, results: platforms} // for the initial data we use our platforms static data, so we don't need to show the loading spinner
+});
 
 export default usePlatforms;
