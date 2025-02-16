8dc84b7e017c45eaaedabf6e5903a42dc253ae77
niklas.edelstam <n.edelstam-ext@catalyse-it.com>
2025-02-16 21:49:11 +0100

    running on server config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// config when running on the server
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+    allowedHosts: ['gamehub.niklasedelstam.dev']
+  },
+})
+/* config when running locally:
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -8,3 +19,4 @@ export default defineConfig({
     host: true,
   },
 })
+*/
